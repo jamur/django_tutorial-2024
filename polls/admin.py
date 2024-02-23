7e40c226555b19951876a8e5915796a0011bc3f8
Rafael Jamur <jamur@ufpr.br>
2024-02-23 18:23:53 +0000

    ubuntu admin correto

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Question, Choice
 # Register your models here.
 
 admin.site.register(Question)
-=======
-from .models import Model1, Model2
-
-admin.site.register(Model1)
-admin.site.register(Model2)
->>>>>>> 66b4a0c785dd6540216c900c690d85758f5b20d6
